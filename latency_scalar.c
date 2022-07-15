4f5e7237040cafbc4998e9506473eb166e191ed1
FilipHa5 <filipha5@wp.pl>
2022-07-15 11:46:32 +0200

    Corrected wrong perf calculation

--- latency_scalar.c
+++ latency_scalar.c
@@ -1,52 +1,60 @@
-#include <stdio.h>
-//#include <stdlib.h>
-//#include<time.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
 #include<sys/time.h>
-//#include<sys/resource.h>
 
+const int num_iter = 100000000;
 
-double time_clock()
-{
-  struct timeval tk;
-  struct timezone tzp;
-  double daytime;
+static clock_t ct;
+static struct timeval tp;
+static struct timezone tzp;
 
-  gettimeofday(&tk, &tzp);
-
-  daytime=(tk.tv_usec)/1e6+tk.tv_sec;
-
-  return(daytime);
+double time_clock(){ 
+    struct timeval tk;
+    struct timezone tzp;
+    double daytime;
+    
+    gettimeofday(&tk, &tzp);
+    
+    daytime=(tk.tv_usec)/1e6+tk.tv_sec;
+    
+    return(daytime);
 }
 
-void main(){
-
+void main(void){
+    double wallclock_time;
+    double a,b,c, sum;
     int i;
-    const int num_iter = 100000000;
-    double a = 1.000001;
-    //double b = 1.000001;
-    //clock_t start, stop;
 
-    //warm up - to increase CPU freq
-    for(i=0; i<10*num_iter; i++){
+    clock_t start, stop;
+
+
+     a = 1.000001;
+     b = 1.000002;
+     c = 1.000003;
+
+     //warm up
+    for(i=0;i<10*num_iter;i++){
         a = 1.00000001*a+0.000001;
     }
+    a *= 0.000000001;
 
     //test
-    double start = time_clock();
-    //start = clock();
-
-    for(i=0; i<num_iter; i++){
-        a = 1.00000001*a+0.000001;
+    start = clock();
+    wallclock_time = time_clock();
+    for(i=0;i<num_iter;i++){
+        a = 1.00000001*a+0.000001; 
     }
-    //stop = clock();
-    double stop = time_clock();
+    stop = clock();
+    wallclock_time = time_clock() - wallclock_time;
 
-    int nr_oper = 1*2*num_iter;
-    double time = (stop-start);///CLOCKS_PER_SEC;
-    float perf = nr_oper*1e-9/stop-start;
+    double nr_oper_local = 1 * 2.0 * num_iter;
+    double time_elapsed = (stop-start)/CLOCKS_PER_SEC;
+    sum = a+b+c;
     
-    printf("SUCCESS - LATENCY TEST FINISHED!\n");
-    printf("Time of execution %i floating point operations: %lf\n", nr_oper, time);
-    printf("Latency - time per operation: %f ns\n", (float)time*10e9/(float)nr_oper);
-    printf("Performance: %f GFLOP/s\n", (float)(nr_oper*1e-9/time));
-    }
+    printf("\nResult of arithmetic operations: %lf\n", sum);
+    printf("Time for executing %lf operations: %lf\n",nr_oper_local, wallclock_time);
+    printf("Performance: %lf GFLOP/s\n\n",nr_oper_local*1.0e-9/wallclock_time);
+
+    
+}
