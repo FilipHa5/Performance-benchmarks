04d823c2d3232f5a56410b286400fd860ecbdcf4
FilipHa5 <filipha5@wp.pl>
2022-08-02 00:06:49 +0200

    Corrected approach

--- mem_line_size.c
+++ mem_line_size.c
@@ -1,61 +1,70 @@
 #include <stdlib.h>
-
 #include <stdio.h>
-
 #include <time.h>
-
 #include <sys/time.h>
+#include <sys/times.h>
+#include <math.h>
 
-#define WORKING_SET_SIZE 4096*100//33554432
+#define WORKING_SET_SIZE 409600 //33554432
 #define STRIDE_MAX 15
 
 static struct timeval tp;
 static struct timezone tzp;
 
-double time_clock() {
+double time_clock()
+{
+	struct timeval tk;
+	struct timezone tzp;
+	double daytime;
 
-  struct timeval tk;
-  struct timezone tzp;
-  double daytime;
+	gettimeofday(&tk, &tzp);
 
-  gettimeofday( & tk, & tzp);
+	daytime = (tk.tv_usec) / 1e6 + tk.tv_sec;
 
-  daytime = (tk.tv_usec) / 1e6 + tk.tv_sec;
-
-  return (daytime);
+	return (daytime);
 }
 
-void main() {
-  long int i, j, k, n, l, m, array_size_iter;
-  long int array_size = WORKING_SET_SIZE;
-  clock_t start, stop;
-  int stride = 1;
-  double wallclock_time;
+void main()
+{
+	long int i, j;
+	long int array_size = WORKING_SET_SIZE;
+	clock_t start, stop;
+	int stride = 1;
+	double wallclock_time;
 
-for (stride; stride<=STRIDE_MAX; stride++){
-    double * array = malloc(array_size * sizeof(double));
-    for (i = 0; i < array_size; i++) {
-      array[i]++;
-    }
-    start = clock();
-    wallclock_time = time_clock();
-    for (j = 0; j < 10000; j++) {
-      for (i = 0; i < array_size; i += stride) {
-        array[i]++;
-      }
-    }
-    stop = clock();
-    wallclock_time = time_clock() - wallclock_time;
-    free(array);
+	for (stride; stride <= STRIDE_MAX; stride++)
+	{
+		double *array = malloc(array_size* sizeof(double));
+    //warmup
+		for (i = 0; i < array_size; i+=8)
+		{
+			array[i]++;
+		}
 
-    double calc_time = wallclock_time; //*/(double)(stop-start)/CLOCKS_PER_SEC;
-    double time = calc_time * 1.0e9 / (array_size/stride);
-    double throughput = sizeof(double) / (time / 100);
+		start = clock();
+		wallclock_time = time_clock();
+		for (i = 0; i < 1000; i++)
+		{
+			for (j = 0; j < array_size; j += stride)
+			{
+				array[j]++;
+			}
+		}
 
-    if (stride == 1){
-      printf("\n\n== WORKING SET SIZE %lf B ==\n\n", (double)(array_size) * sizeof(double));
-    }
-    printf("Stride %i, Time (strided): %lf ns, " , stride, time);
-    printf("Throughput: %lf B/s, \n", throughput);
-}
+		stop = clock();
+		wallclock_time = time_clock() - wallclock_time;
+		free(array);
+
+		//double calc_time = (double)(stop-start)/CLOCKS_PER_SEC;
+		//double throughput = (array_size * sizeof(double) * 1000)*9.31*1e-10/ wallclock_time;
+    long int nr_accesses =  1000 * (WORKING_SET_SIZE/stride);
+
+		if (stride == 1)
+		{
+			printf("\n\n== WORKING SET SIZE %i B ==\n\n", (int)array_size *sizeof(double));
+		}
+
+		printf("Stride %i, Time per accesses: %lf ns \n", stride, wallclock_time*1.0e9 / nr_accesses);
+		//printf("Throughput: %lf GB/s, \n", throughput);
+	}
 }
