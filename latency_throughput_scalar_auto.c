2ca1536e170c877cc2ccc95441b1bd4bbcb31921
FilipHa5 <filipha5@wp.pl>
2022-08-04 15:30:36 +0200

    A bit of automation - needs to be corrected

--- latency_throughput_scalar_auto.c
+++ latency_throughput_scalar_auto.c
@@ -4,6 +4,7 @@
 #include<sys/time.h>
 
 const int num_iter = 100000000;
+const int num_var = 1;
 
 static clock_t ct;
 static struct timeval tp;
@@ -24,33 +25,48 @@ double time_clock(){
 void main(void){
     double wallclock_time;
     double a,b,c, sum;
-    int i;
+    double val = 1.000000;
+    double add = 0.000001;
+    int i, j;
+    int start_size = 10;
 
     clock_t start, stop;
 
-
-     a = 1.000001;
-     b = 1.000002;
-     c = 1.000003;
-
-     //warm up
-    for(i=0;i<10*num_iter;i++){
-        a = 1.00000001*a+0.000001;
+    double* array = malloc(start_size * sizeof(double));
+    
+    //fill the array
+    for(i = 0; i<start_size; i++){
+        array[i] = val + add;
+        add += 0.000001;
     }
-    a *= 0.000000001;
+    add = 0.000001;
+
+    //warm up
+    for(i = 0; i<start_size; i++){
+        array[i] = val * array[i] + add;
+    }
+    //reset
+    for(i = 0; i<10000; i++){
+        array[0] = 1.00000001*array[0]+0.000001;
+    }
+    array[0] *= 0.000000001;
 
     //test
-    start = clock();
     wallclock_time = time_clock();
-    for(i=0;i<num_iter;i++){
-        a = 1.00000001*a+0.000001; 
+    for(i = 0; i<num_iter; i++){
+        for(j=0; j<num_var; j++){
+            array[j] = val * array[j] + add;
+        }
     }
-    stop = clock();
     wallclock_time = time_clock() - wallclock_time;
 
-    double nr_oper_local = 1 * 2.0 * num_iter;
+    //sum
+    for(i = 0; i<start_size; i++){
+        sum += array[i];
+    }
+
+    double nr_oper_local = num_var * 2.0 * num_iter;
     double time_elapsed = (stop-start)/CLOCKS_PER_SEC;
-    sum = a+b+c;
     
     printf("\nResult of arithmetic operations: %lf\n", sum);
     printf("Time for executing %lf operations: %lf\n",nr_oper_local, wallclock_time);
