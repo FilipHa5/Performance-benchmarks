7e91b8cebb0fe34e349cc299193298faa65c583a
FilipHa5 <filipha5@wp.pl>
2022-08-25 13:43:27 +0200

    Added new features

--- latency_scalar.c
+++ latency_scalar.c
@@ -13,11 +13,11 @@ double time_clock(){
     struct timeval tk;
     struct timezone tzp;
     double daytime;
-    
+
     gettimeofday(&tk, &tzp);
-    
+
     daytime=(tk.tv_usec)/1e6+tk.tv_sec;
-    
+
     return(daytime);
 }
 
@@ -33,7 +33,7 @@ void main(void){
      b = 1.000002;
      c = 1.000003;
 
-     //warm up
+    //warm up
     for(i=0;i<10*num_iter;i++){
         a = 1.00000001*a+0.000001;
     }
@@ -49,12 +49,12 @@ void main(void){
     wallclock_time = time_clock() - wallclock_time;
 
     double nr_oper_local = 1 * 2.0 * num_iter;
-    double time_elapsed = (stop-start)/CLOCKS_PER_SEC;
+    double time_elapsed = (stop-start) / CLOCKS_PER_SEC;
+    double latency = wallclock_time * 1.0e9 / nr_oper_local;
     sum = a+b+c;
-    
-    printf("\nResult of arithmetic operations: %lf\n", sum);
-    printf("Time for executing %lf operations: %lf\n",nr_oper_local, wallclock_time);
-    printf("Performance: %lf GFLOP/s\n\n",nr_oper_local*1.0e-9/wallclock_time);
 
-    
+    printf("\nResult of arithmetic operations: %lf\n", sum);
+    printf("Time for executing %lf operations: %lf s\n",nr_oper_local, wallclock_time);
+    printf("Latency: %lf ns\n", latency);
+    printf("Performance: %lf GFLOP/s\n\n",nr_oper_local*1.0e-9/wallclock_time);
 }
